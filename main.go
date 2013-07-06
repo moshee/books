9552d501c53ff8eed2b2a225ebb551b6766356de
moshee <moshee@displaynone.us>
2013-07-06 11:29:18 +0000

    temporary local static handler until I figure out a cdn-type thing

--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"github.com/moshee/gas"
 )
 
+func StaticHandler(g *gas.Gas) {
+	path := g.Args["path"]
+	f, err := os.Open(filepath.Join("./static", path))
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	io.Copy(g.ResponseWriter, f)
+}
+
 func main() {
 	gas.New().
+		Get("/static/{path}", StaticHandler).
 		Get("/", books.Index)
 
-	err := gas.Ignition()
-	if err != nil {
-		gas.Log(gas.Fatal, err)
-	}
+	gas.Ignition()
 }
