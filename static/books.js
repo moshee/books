f2e7b168365941948c29030ee24eef2c84b6156e
moshee <moshee@displaynone.us>
2013-09-26 10:27:21 -0700

    minor layout changes

--- static/books.js
+++ static/books.js
@@ -1,4 +1,6 @@
 (function() {
+	// utils
+	//
 	function $(sel, base) {
 		if (base == null) base = document;
 		return base.querySelector(sel);
@@ -9,11 +11,37 @@
 		return base.querySelectorAll(sel);
 	}
 
+	function ajax_post(path, data, async, callback) {
+		var x = new XMLHttpRequest();
+		if (typeof callback !== 'undefined') {
+			x.addEventListener('load', callback, true);
+		}
+
+		x.open('post', path, async);
+
+		if (typeof data === 'object') {
+			var fd = new FormData();
+			for (var key in data) {
+				fd.append(key, data[key]);
+			}
+
+			x.send(fd);
+		} else {
+			x.send(data);
+		}
+	}
+
+	// search
+
 	// pjax that shit up
 	// replace the section#main content and transition that shit in
 	// with some slick animations
-	function doSearch() {
-		s = [];
+	function doSearch(e) {
+		e.target.setAttribute('disabled');
+		var old = e.target.innerHTML;
+		e.target.innerHTML = 'Please wait...';
+
+		var s = {};
 		var lis = $$('li', searchFilter);
 		for (var i = 0, li; li = lis[i]; i++) {
 			var input = $('input', li).value;
@@ -21,11 +49,18 @@
 				continue;
 			}
 			var select = $('select', li).value;
-			var o = {};
-			o[select] = input;
-			s.push(o);
+
+			if (typeof s[select] === 'undefined') {
+				s[select] = [input];
+			} else {
+				s[select].push(input);
+			}
 		}
-		console.log(s);
+
+		ajax_post('/search', s, true, function(x) {
+			e.target.removeAttribute('disabled');
+			e.target.innerHTML = old;
+		});
 	}
 
 	var searchFilter, searchFilterItem;
@@ -47,6 +82,16 @@
 		li.parentNode.removeChild(li);
 	}
 
+	// login
+	
+	function login(e) {
+		e.target.setAttribute('disabled');
+		e.target.innerText = 'Logging in...';
+
+		var form = e.target.parentElement;
+		
+	}
+
 	function main() {
 		searchFilter = $('#search ul');
 		searchFilterItem = $('li', searchFilter); 
@@ -55,6 +100,8 @@
 		$('button.remove-filter', searchFilterItem)
 			.addEventListener('click', delSearchFilterItem, false);
 		$('#search-button').addEventListener('click', doSearch, false);
+
+		$('#login-button').addEventListener('click', login, false);
 
 		var lr = document.createElement('script');
 		lr.async = true;
